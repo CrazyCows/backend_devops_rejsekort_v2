27549a3bd0340d422956a0a02e6f2000fd8c063b
CrazyCows <emil.s.simonsen@gmail.com>
2024-11-22 23:23:53 +0100

    as

--- Startup.cs
+++ Startup.cs
@@ -26,7 +26,7 @@ namespace backend_devops_rejsekort_v2
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<UserContext>(options =>
-                options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
+                options.UseNpgsql("Host=localhost;Port=5432;Database=mydb;Username=myuser;Password=mypassword"));
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
             .AddEntityFrameworkStores<UserContext>()
