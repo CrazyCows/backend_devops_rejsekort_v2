8c478ab652ab53f06f10b0d922f1d48b1b8b1647
CrazyCows <emil.s.simonsen@gmail.com>
2024-11-22 19:34:53 +0100

    implementing github flow

---
+++ .github/workflows/dotnet.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace backend_devops_rejsekort_v2.github.workflows
+{
+    internal class dotnet
+    {
+    }
+}
